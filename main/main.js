ced53dc01a48f2f819852ca4f7ccdcb1c6680574
Salvatore Lauriola <salvatore6324@gmail.com>
2020-03-26 17:37:10 +0100

    project finished

--- main/main.js
+++ main/main.js
@@ -8,31 +8,30 @@ var age = parseInt(prompt('Quanti anni hai?'));
 
 // 3 Calcolare prezzo totale del viaggio sapendo che costo del biglietto = 0.21 € al km;
 
-var price = ( km * 0.21 );
-
-var final_price;
-
-
-// 4 Applicare sconto del 20 % per i minorenni;
+var price = km * 0.21 ;
+var final_price = 0;
+var message = 'Questo è il prezzo finale: ';
+if (age <18 || age >65) {
+  message = 'Questo è il prezzo scontato: ';
+} else {
+  
+};
 
 if (age <18 ) {
-    final_price = (price - ( price * 20 / 100)); + 'Questo è il prezzo scontato';
+    final_price = (price - ( price * 20 / 100));
     console.log(final_price.toFixed(2));
 } else if (age > 65 ) {
-    final_price = (price - ( price * 40 / 100)) + 'Questo è il prezzo scontato';
+    final_price = (price - ( price * 40 / 100));
     console.log(final_price.toFixed(2));
 }
-    else {
-      var totale = price;
-      console.log(final_price.toFixed(2));
+  else {
+    final_price=price;
+    console.log(final_price.toFixed(2));
 }
 
-document.getElementById('ticket-price').innerHTML = final_price.toFixed(2);
-
-// 5 Applicare sconto del 40 % per gli over 65;
+document.getElementById('ticket-price').innerHTML = message + final_price.toFixed(2);
 
 
 
 
-// 6 Mostrare a schermo il prezzo del biglietto specificando se è stato applicato un eventuale sconto.
 
