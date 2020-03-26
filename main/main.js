1868574f8ae13c578df3c38ef8a3a21d6f556ee9
Salvatore Lauriola <salvatore6324@gmail.com>
2020-03-26 15:09:03 +0100

    Inizializzazione progetto

---
+++ main/main.js
@@ -0,0 +1,12 @@
+// 1 Chiedere quanti km vuole percorrere il passeggero;
+
+// 2 Chiedere quanti anni ha il passeggero;
+
+// 3 Calcolare prezzo totale del viaggio sapendo che costo del biglietto = 0.21 €;
+
+// 4 Applicare sconto del 20 % per i minorenni;
+
+// 5 Applicare sconto del 40 % per gli over 65;
+
+// 6 Mostrare a schermo il prezzo del biglietto specificando se è stato applicato un eventuale sconto.
+
